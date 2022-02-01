e9b7cfe6cea06077d1981da81a5b3e0762cfc106
Hasyna <hasycadar@gmail.com>
2022-02-01 11:28:33 +0530

    method3

--- src/org/git/sec/GitSecond.java
+++ src/org/git/sec/GitSecond.java
@@ -1,5 +1,12 @@
 package org.git.sec;
 
 public class GitSecond {
-
+	
+private void m1() {
+	System.out.println("kijhu6ygt5frd4e");
+}
+	public static void main(String[] args) {
+		GitSecond to = new GitSecond();
+		to.m1();
+}
 }
